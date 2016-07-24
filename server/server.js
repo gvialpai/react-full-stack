9344f4d1d55c6ab00cd0a415d05dc459d9c3013e
gvialpai <vialpg@gmail.com>
2016-07-24 10:13:14 +0100

    Implementing backend, db linked to front-end

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 var path = require("path");
 
+//controllers
+var schoolController = require("./controllers/schoolController");
+
+//Express request pipeline
 var app = express();
-app.use(express.static(path.join(__dirname,"../app/dist")));
-app.listen(3000,function(){
+app.use(express.static(path.join(__dirname, "../app/dist")));
+app.use(bodyParser.json())
+app.use("/api", schoolController);
+
+app.listen(3000, function () {
     console.log("Started listening on port", 3000);
-})
+});
+
+
+// Connect to mongodb database
+mongoose.connect("mongodb://localhost/schoolfinder");
